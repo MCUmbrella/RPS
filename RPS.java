a9a7905bdc552ec0803a35c94a0b42ecc581bcbe
MCUmbrella <40854260+MCUmbrella@users.noreply.github.com>
2018-07-23 10:53:34 +0800

    Update RPS.java

--- RPS.java
+++ RPS.java
@@ -6,44 +6,80 @@ public class RPS
 	
 	public static void main(String[] args) 
 	{
-		System.out.println("(C) 2018 Umbrella Studio. Rock-Paper-Scissors Simulator V1.2");
+		System.out.println("(C) 2018 Umbrella Studio. Rock-Paper-Scissors Simulator V1.8");
 		Scanner in=new Scanner(System.in);
 		Random r=new Random();
-		long pw=0;
-		long cw=0;
+		long pw=0L;
+		long cw=0L;
+		long t=0L;
+		long d=0L;
 		System.out.print("Enter your name:___________\b\b\b\b\b\b\b\b\b\b");
 		String name=in.nextLine();
 		if(name.equals("")){System.out.println("[!] No name detected. Exiting...");System.exit(-1);}
-		System.out.println("Welcome, "+name+".");
+		
 		while(true)
 		{
-			System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
-			System.out.println("Please input rock:1, scissors:2, paper:3,");
-			System.out.print("["+pw+":"+cw+"]"+"      Exit & Statistics:4. [_]\b\b");
-			int p=in.nextInt();
-			if(p==4) {System.out.println(name+" won "+pw+" times.");System.out.println("Computer won "+cw+" times.");System.out.println("-Exiting...");System.exit(0);}
-			if(p==1||p==2||p==3) 
+			System.out.println("");
+			System.out.println("");
+			System.out.println("");
+			System.out.println("");
+			System.out.println("");
+			System.out.println(" _______________________ ");
+			System.out.println("|  Rock-Paper-Scissors  |");
+			System.out.println(" ----------------------- ");
+			System.out.println("Welcome, "+name+".");
+			System.out.println("1-Play 2-Statistics");
+			System.out.print("3-Exit          [_]\b\b");
+			int a=in.nextInt();
+			if(a==1) 
 			{
-				if(p==1) {System.out.println("-You have chosen the rock, please wait for the computer to choose...");}
-				if(p==2) {System.out.println("-You have chosen scissors, please wait for the computer to choose...");}
-				if(p==3) {System.out.println("-You have chosen the paper, please wait for the computer to choose...");}
-				
-				int c=r.nextInt(3);
-				if(c==0) {System.out.println("-Computer chose the rock.");}
-				if(c==1) {System.out.println("-Computer chose scissors.");}
-				if(c==2) {System.out.println("-Computer chose the paper.");}
-				
-				if(p==1&&c==0) {System.out.println(name+" and computer draw.");}
-				if(p==2&&c==1) {System.out.println(name+" and computer draw.");}
-				if(p==3&&c==2) {System.out.println(name+" and computer draw.");}
-				if(p==2&&c==0) {System.out.println("Computer wins.");cw++;}
-				if(p==1&&c==1) {System.out.println(name+" wins.");pw++;}
-				if(p==1&&c==2) {System.out.println("Computer wins.");cw++;}
-				if(p==2&&c==2) {System.out.println(name+" wins.");pw++;}
-				if(p==3&&c==0) {System.out.println(name+" wins.");pw++;}
-				if(p==3&&c==1) {System.out.println("Computer wins.");cw++;}
-				
-			}else {System.out.println("[!] Incorrect input number. Exiting...");System.out.println(name+" won "+pw+" times.");System.out.println("Computer won "+cw+" times.");System.exit(-1);}
+				while(true)
+				{
+					System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+					System.out.println("Please input rock:1, scissors:2, paper:3,");
+					System.out.print("["+pw+":"+cw+"]"+"      Exit & Statistics:4. [_]\b\b");
+					int p=in.nextInt();
+					
+					if(p==1||p==2||p==3) 
+					{
+						if(p==1) {System.out.println("-You have chosen the rock, please wait for the computer to choose...");}
+						if(p==2) {System.out.println("-You have chosen scissors, please wait for the computer to choose...");}
+						if(p==3) {System.out.println("-You have chosen the paper, please wait for the computer to choose...");}
+						
+						int c=r.nextInt(3);
+						if(c==0) {System.out.println("-Computer chose the rock.");}
+						if(c==1) {System.out.println("-Computer chose scissors.");}
+						if(c==2) {System.out.println("-Computer chose the paper.");}
+						
+						if(p==1&&c==0) {System.out.println(name+" and computer draw.");d++;}
+						if(p==2&&c==1) {System.out.println(name+" and computer draw.");d++;}
+						if(p==3&&c==2) {System.out.println(name+" and computer draw.");d++;}
+						if(p==2&&c==0) {System.out.println("Computer wins.");cw++;}
+						if(p==1&&c==1) {System.out.println(name+" wins.");pw++;}
+						if(p==1&&c==2) {System.out.println("Computer wins.");cw++;}
+						if(p==2&&c==2) {System.out.println(name+" wins.");pw++;}
+						if(p==3&&c==0) {System.out.println(name+" wins.");pw++;}
+						if(p==3&&c==1) {System.out.println("Computer wins.");cw++;}
+						t++;
+						
+					}else if(p==4) {System.out.println(name+" won "+pw+" times.");System.out.println("Computer won "+cw+" times.");System.out.println("Draw "+d+" times.");System.out.println("-Exiting...");break;}else {System.out.println("[!] Incorrect input number. Exiting...");System.out.println(name+" won "+pw+" times.");System.out.println("Computer won "+cw+" times.");System.exit(-1);}
+				}
+			}else if(a==2) 
+			{
+				System.out.println(name+" won "+pw+" times.");
+				System.out.println("Computer won "+cw+" times.");
+				System.out.println("Draw "+d+" times.");
+				System.out.println("Total rounds: "+t);
+				if(t!=0)
+				{
+				float w=(float)(pw*100/t);
+				System.out.println("Win rate: "+w+" %");
+				}
+			}else if(a==3) 
+			{
+				System.out.println("Bye!");System.exit(0);
+			}
+			
 		}
 	}
 	
